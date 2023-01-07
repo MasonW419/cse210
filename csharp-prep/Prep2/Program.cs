5f91699cad5f8166dcc5275c0696740dc602cdb2
MasonW419 <mwilkins419@gmail.com>
2023-01-07 13:14:55 -0700

    Completed Prep Activites 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        int grade_percent = Convert.ToInt32(Console.ReadLine());
+        string grade_let = "";
+        if (grade_percent >= 90)
+        {
+            grade_let = "A";
+        }
+        else if (grade_percent >= 80)
+        {
+            grade_let = "B";
+        } 
+        else if (grade_percent >= 70)
+        {
+            grade_let = "C";
+        } 
+        else if (grade_percent >= 60)
+        {
+            grade_let = "D";
+        } 
+        else
+        {
+            grade_let = "F";
+        } 
+        Console.WriteLine($"Your grade is {grade_let}");
     }
 }
