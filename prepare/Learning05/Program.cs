134c814785d6e3bf53f225a38bca454925167b3e
MasonW419 <mwilkins419@gmail.com>
2023-03-04 10:53:30 -0700

    completed polymorphism activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("blue", 5));
+        shapes.Add(new Rectangle("red", 4, 6));
+        shapes.Add(new Circle("yellow", 3));
+        foreach(Shape s in shapes)
+        {
+            Console.WriteLine(s.GetColor());
+            Console.WriteLine(s.GetArea());
+        }
     }
 }
